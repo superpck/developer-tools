a84393f19cdf1afa4e445e627dc25c1d0e38bab5
superpck <superpck@gmail.com>
2026-06-08 12:17:10 +0700

    cli 22.0.0

--- src/app/components/send-request/send-request.ts
+++ src/app/components/send-request/send-request.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-send-request',
   imports: [],
   templateUrl: './send-request.html',
+  changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './send-request.css',
 })
 export class SendRequest {
